3fd02eb8afa300d8d2bdd6f968fbaa90a1059f35
amirakbarzade <88921274+amirakbarzade@users.noreply.github.com>
2023-05-28 16:13:33 +0330

    Upload The first project with Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/index.html"],
+  theme: {
+    colors: {
+      blue: "#ff0000",
+    },
+   
+  },
+  plugins: [],
+}
+
