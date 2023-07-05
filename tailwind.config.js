1ae66a456c67d59330fb37555008f059ca19143b
amirakbarzade <88921274+amirakbarzade@users.noreply.github.com>
2023-07-05 14:33:49 +0330

    rtl

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: ["./src/index.html"],
   theme: {
     colors: {
-      blue: "#ff0000",
+     
     },
    
   },
